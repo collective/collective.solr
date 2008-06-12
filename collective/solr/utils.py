218ffdcf691e312bd38b61d901a664d997a4fb29
Andreas Zeidler <zeidler@jarn.com>
2008-06-12 22:04:10 +0000

    change walker to traverse the hierarchy breadth-first, so that indexing remains in the "neighborhood" and doesn't unnecessarily wake up different parent objects all the time

--- collective/solr/utils.py
+++ collective/solr/utils.py
@@ -33,10 +33,10 @@ def findObjects(origin):
     base = '/'.join(origin.getPhysicalPath())
     cut = len(base) + 1
     paths = [ base ]
-    for path in paths:
+    for idx, path in enumerate(paths):
         obj = traverse(path)
         yield path[cut:], obj
         if hasattr(aq_base(obj), 'objectIds'):
             for id in obj.objectIds():
-                paths.append(path + '/' + id)
+                paths.insert(idx + 1, path + '/' + id)
 
