fcf3608362209af790f5d98695085b0bcd24f6df
wichert <wichert>
2008-06-19 11:15:11 +0000

    Have to strip here or a the newline gets turned into a trailing -

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from os.path import join
 
-version = open(join('collective', 'solr', 'version.txt')).read()
+version = open(join('collective', 'solr', 'version.txt')).read().strip()
 readme = open("README.txt").read()
 history = open(join('docs', 'HISTORY.txt')).read()
 
