ae36a909a56df3cb9a1ba524659450d4ba9e37d0
Andreas Zeidler <zeidler@jarn.com>
2009-03-12 18:50:39 +0000

    add a (dummy) view for displaying facet information

--- collective/solr/browser/facets.py
+++ collective/solr/browser/facets.py
@@ -1,6 +1,12 @@
+from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import SearchBoxViewlet
 
 
 class SearchBox(SearchBoxViewlet):
     index = ViewPageTemplateFile('searchbox.pt')
+
+
+class SearchFacetsView(BrowserView):
+    """ view for displaying facetting info as provided by solr searches """
+
