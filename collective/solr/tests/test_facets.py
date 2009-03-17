c62f495975360d4a9d8f443702957375cf56aaab
Andreas Zeidler <zeidler@jarn.com>
2009-03-17 16:05:24 +0000

    avoid running tests repeatedly by not inheriting from another test case

--- collective/solr/tests/test_facets.py
+++ collective/solr/tests/test_facets.py
@@ -4,15 +4,25 @@ from zope.component import getMultiAdapter
 from zope.publisher.browser import TestRequest
 
 from collective.solr.tests.utils import pingSolr
-from collective.solr.tests.test_server import SolrServerTests
+from collective.solr.tests.base import SolrTestCase
 from collective.solr.browser.interfaces import IThemeSpecific
 from collective.solr.dispatcher import solrSearchResults
+from collective.solr.utils import activate
 
 
-class SolrFacettingTests(SolrServerTests):
+class SolrFacettingTests(SolrTestCase):
+
+    def afterSetUp(self):
+        activate()
+        self.portal.REQUEST.RESPONSE.write = lambda x: x    # ignore output
+        self.maintenance = self.portal.unrestrictedTraverse('solr-maintenance')
+        self.maintenance.clear()
+        self.maintenance.reindex()
+
+    def beforeTearDown(self):
+        activate(active=False)
 
     def testFacettedSearchWithKeywordArguments(self):
-        self.maintenance.reindex()
         results = solrSearchResults(SearchableText='News', facet='true',
             facet_field='portal_type')
         self.assertEqual([r.physicalPath for r in results],
@@ -23,7 +33,6 @@ class SolrFacettingTests(SolrServerTests):
         self.assertEqual(types['Topic'], 1)
 
     def testFacettedSearchWithRequestArguments(self):
-        self.maintenance.reindex()
         request = TestRequest()
         request.form['SearchableText'] = 'News'
         request.form['facet'] = 'true'
@@ -42,7 +51,6 @@ class SolrFacettingTests(SolrServerTests):
             html = html[position:]
 
     def testFacetsInformationView(self):
-        self.maintenance.reindex()
         request = TestRequest()
         request.form['SearchableText'] = 'News'
         request.form['facet'] = 'true'
