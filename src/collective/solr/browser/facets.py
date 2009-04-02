0b3608f8b34b5fb07c80e65b13f3a33cc6dcaa97
Andreas Zeidler <zeidler@jarn.com>
2009-04-02 16:00:43 +0000

    display already selected facets along with links to remove them again

--- src/collective/solr/browser/facets.py
+++ src/collective/solr/browser/facets.py
@@ -2,6 +2,7 @@ from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import SearchBoxViewlet
 from collective.solr.browser.utils import facetParameters, convertFacets
+from urllib import urlencode
 
 
 class SearchBox(SearchBoxViewlet):
@@ -30,3 +31,22 @@ class SearchFacetsView(BrowserView):
                 self.context, self.request.form)
         else:
             return None
+
+    def selected(self):
+        """ determine selected facets and prepare links to clear them;
+            this assumes that facets are selected using filter queries """
+        info = []
+        facets = self.request.form.get('facet.field', [])
+        if isinstance(facets, basestring):
+            facets = [facets]
+        fq = self.request.form.get('fq', [])
+        if isinstance(fq, basestring):
+            fq = [fq]
+        for idx, query in enumerate(fq):
+            field, value = query.split(':', 1)
+            params = self.request.form.copy()
+            params['fq'] = fq[:idx] + fq[idx+1:]
+            if field not in facets:
+                params['facet.field'] = facets + [field]
+            info.append(dict(title=field, query=urlencode(params, doseq=True)))
+        return info
--- src/collective/solr/tests/test_browserutils.py
+++ src/collective/solr/tests/test_browserutils.py
@@ -1,9 +1,11 @@
 from unittest import TestCase, defaultTestLoader, main
 from zope.component import provideUtility, getGlobalSiteManager
+from zope.publisher.browser import TestRequest
 from collective.solr.tests.utils import getData
 from collective.solr.interfaces import ISolrConnectionConfig
 from collective.solr.manager import SolrConnectionConfig
 from collective.solr.parser import SolrResponse
+from collective.solr.browser.facets import SearchFacetsView
 from collective.solr.browser.utils import facetParameters, convertFacets
 from urllib import unquote
 
@@ -149,6 +151,46 @@ class FacettingHelperTest(TestCase):
             'fq=bar:private', 'fq=foo:Document'])
         self.assertEqual(params(counts[1]['query']), [
             'fq=bar:private', 'fq=foo:Folder'])
+
+    def testSelectedFacetsInformation(self):
+        request = TestRequest()
+        selected = SearchFacetsView(Dummy(), request).selected
+        # initially no facets are seleted
+        self.assertEqual(selected(), [])
+        # so let's select one...
+        params = lambda query: sorted(map(unquote, query.split('&')))
+        info = lambda: [(i['title'], params(i['query'])) for i in selected()]
+        request.form['fq'] = 'foo:xy'
+        self.assertEqual(info(), [
+            ('foo', ['facet.field=foo']),
+        ])
+        # and then some more...
+        request.form['fq'] = ['foo:x', 'bar:y']
+        self.assertEqual(info(), [
+            ('foo', ['facet.field=foo', 'fq=bar:y']),
+            ('bar', ['facet.field=bar', 'fq=foo:x']),
+        ])
+        request.form['fq'] = ['foo:x', 'bar:y', 'bah:z']
+        self.assertEqual(info(), [
+            ('foo', ['facet.field=foo', 'fq=bah:z', 'fq=bar:y']),
+            ('bar', ['facet.field=bar', 'fq=bah:z', 'fq=foo:x']),
+            ('bah', ['facet.field=bah', 'fq=bar:y', 'fq=foo:x']),
+        ])
+        # extra parameter should be left untouched
+        request.form['foo'] = 'bar'
+        self.assertEqual(info(), [
+            ('foo', ['facet.field=foo', 'foo=bar', 'fq=bah:z', 'fq=bar:y']),
+            ('bar', ['facet.field=bar', 'foo=bar', 'fq=bah:z', 'fq=foo:x']),
+            ('bah', ['facet.field=bah', 'foo=bar', 'fq=bar:y', 'fq=foo:x']),
+        ])
+        # an existing 'facet.field' parameter should be preserved
+        del request.form['foo']
+        request.form['facet.field'] = 'x'
+        self.assertEqual(info(), [
+            ('foo', ['facet.field=foo', 'facet.field=x', 'fq=bah:z', 'fq=bar:y']),
+            ('bar', ['facet.field=bar', 'facet.field=x', 'fq=bah:z', 'fq=foo:x']),
+            ('bah', ['facet.field=bah', 'facet.field=x', 'fq=bar:y', 'fq=foo:x']),
+        ])
 
 
 def test_suite():
