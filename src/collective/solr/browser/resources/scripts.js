0b5bd596342e3549a985f4cee9f15c87ea500cc2
Florian Schulze <fschulze@jarn.com>
2009-05-26 10:14:29 +0000

    Use proper way of accessing jQuery. Fix IE6 syntax error (no trailing commas in dicts allowed).

--- src/collective/solr/browser/resources/scripts.js
+++ src/collective/solr/browser/resources/scripts.js
@@ -1,8 +1,9 @@
 /* use `collective.js.showmore` to hide facet counts via jquery */
-
-jq(function() {
-    jq('#portal-searchfacets dl').showMore({
+(function($) {
+$(function() {
+    $('#portal-searchfacets dl').showMore({
         expression: 'dt:gt(4), dd:gt(4)',
-        grace_count: 2,
+        grace_count: 2
     });
 });
+})(jQuery);
