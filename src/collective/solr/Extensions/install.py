8664f0b2fb6018bebee2e070bb8699eba23fd5f3
Hanno Schlichting <hanno@jarn.com>
2010-07-08 14:14:52 +0000

    Revert c120183. Both utilities and confligets are tracked by quick installer. There's no need for a custom uninstall profile here.

--- src/collective/solr/Extensions/install.py
+++
@@ -1,7 +0,0 @@
-
-from Products.CMFCore.utils import getToolByName
-
-def uninstall(self):
-    portal_setup = getToolByName(self, 'portal_setup')
-    portal_setup.runAllImportStepsFromProfile(
-            'profile-collective.solr:uninstall')
