e7bb3bf1ee3eaf58082f556994d0279597f19eaa
Hanno Schlichting <hanno@jarn.com>
2011-07-18 14:24:24 +0200

    Avoid deprecated import locations

--- src/collective/solr/tests/layer.py
+++ src/collective/solr/tests/layer.py
@@ -1,6 +1,6 @@
 from Testing.ZopeTestCase import installPackage, installProduct
-from Products.Five import zcml
-from Products.Five import fiveconfigure
+from Zope2.App import zcml
+
 from collective.testcaselayer.ptc import BasePTCLayer, ptc_layer
 
 
@@ -8,16 +8,11 @@ class SolrLayer(BasePTCLayer):
     """ layer for solr integration tests """
 
     def afterSetUp(self):
-        # load zcml
-        fiveconfigure.debug_mode = True
         from collective import indexing, solr
         zcml.load_config('configure.zcml', indexing)
         zcml.load_config('configure.zcml', solr)
-        fiveconfigure.debug_mode = False
-        # install package, import profile...
         installPackage('collective.indexing', quiet=True)
         installPackage('collective.solr', quiet=True)
-        # finally load the testing profile
         self.addProfile('collective.solr:search')
 
 layer = solr = SolrLayer(bases=[ptc_layer])
@@ -27,11 +22,8 @@ class BlobLinguaLayer(BasePTCLayer):
     """ layer for integration tests with LinguaPlone """
 
     def afterSetUp(self):
-        # load zcml
-        fiveconfigure.debug_mode = True
         from Products import LinguaPlone
         zcml.load_config('configure.zcml', LinguaPlone)
-        fiveconfigure.debug_mode = False
         installProduct('LinguaPlone', quiet=True)
         self.addProfile('LinguaPlone:default')
 
