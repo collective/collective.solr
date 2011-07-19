197c134dcd9166ac50bbcc25556d9ceb7082e1fc
Hanno Schlichting <hanno@jarn.com>
2011-07-19 16:31:33 +0200

    Use new c.indexing release

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(name = 'collective.solr',
       install_requires=[
         'Acquisition',
         'archetypes.schemaextender',
-        'collective.indexing >= 2.0a1',
+        'collective.indexing >= 2.0a2',
         'collective.js.showmore',
         'DateTime',
         'elementtree',
