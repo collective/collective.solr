671170a2c19b24926c7ae801273205976d0d62ad
Hanno Schlichting <hanno@jarn.com>
2011-12-07 13:39:01 +0100

    Removed `solr_dump_catalog` and `solr_import_dump` command line scripts. They were too dependent on internals and had subtle bugs.

--- setup.py
+++ setup.py
@@ -73,7 +73,5 @@ setup(name = 'collective.solr',
         target = plone
         [zopectl.command]
         solr_clear_index = collective.solr.commands:solr_clear_index
-        solr_dump_catalog = collective.solr.commands:solr_dump_catalog
-        solr_import_dump = collective.solr.commands:solr_import_dump
       ''',
 )
