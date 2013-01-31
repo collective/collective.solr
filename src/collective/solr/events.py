10a296675a6ec75e1d3757fedab4ca1f06d74934
Patrick Gerken <do3cc@patrick-gerken.de>
2013-01-31 19:06:51 +0100

    So many errors

--- src/collective/solr/events.py
+++ src/collective/solr/events.py
@@ -1,6 +1,14 @@
-def reorderedEvent(obj, event):
-    mtool = getToolByName(obj, 'portal_membership')
-    if mtool.checkPermission(ModifyPortalCOntent, parent):
-        for child in obj.objectValues():
-            if isinstance(child, CatalogMultiPlex) or isinstance(child, CMFCatalogAware):
-                child.reindexchildect(['getchildPositionInParent'])
+from Products.CMFCore.utils import getToolByName
+from Products.CMFDefault.permissions import ModifyPortalContent
+
+from Products.CMFCore.CMFCatalogAware import CMFCatalogAware
+from Products.Archetypes.CatalogMultiplex import CatalogMultiplex
+
+def reorderedEvent(event):
+    parent = event.object
+    mtool = getToolByName(parent, 'portal_membership')
+    if mtool.checkPermission(ModifyPortalContent, parent):
+        for child in parent.objectValues():
+            if isinstance(child, CatalogMultiplex) or \
+                    isinstance(child, CMFCatalogAware):
+                child.reindexObject(['getObjPositionInParent'])
