0806263508f9213d8e57da236a9b8f739881ebfc
Andreas Zeidler <az@zitc.de>
2013-08-27 12:25:29 +0200

    borrow zcml helper from https://github.com/plone/plone.app.async/commit/ca8c7653 and properly clean up the registry to avoid test isolation errors introduced by 6ccbd9af

--- src/collective/solr/tests/test_integration.py
+++ src/collective/solr/tests/test_integration.py
@@ -2,16 +2,18 @@ from unittest import defaultTestLoader
 from collective.solr.tests.base import SolrTestCase
 
 # test-specific imports go here...
-from zope.component import queryUtility, getUtilitiesFor
+from zope.component import queryUtility, getUtilitiesFor, getGlobalSiteManager
 from Products.CMFCore.utils import getToolByName
 from collective.indexing.interfaces import IIndexQueueProcessor
 from collective.solr.interfaces import ISolrConnectionConfig
 from collective.solr.interfaces import ISolrConnectionManager
 from collective.solr.interfaces import ISolrIndexQueueProcessor
 from collective.solr.interfaces import ISearch
+from collective.solr.interfaces import IZCMLSolrConnectionConfig
 from collective.solr.mangler import mangleQuery
 from collective.solr.exceptions import SolrInactiveException
 from collective.solr.tests.utils import getData, fakehttp, fakeServer
+from collective.solr.tests.utils import loadZCMLString
 from transaction import commit
 from socket import error, timeout
 from time import sleep
@@ -186,9 +188,22 @@ class SiteSearchTests(SolrTestCase):
         self.assertEqual(responses, [])
         self.assertEqual(len(schema), 21)   # 21 items defined in schema.xml
 
+
+class ZCMLSetupTests(SolrTestCase):
+
+    def beforeTearDown(self):
+        manager = queryUtility(ISolrConnectionManager)
+        manager.setHost(active=False)
+        zcmlconfig = queryUtility(IZCMLSolrConnectionConfig)
+        gsm = getGlobalSiteManager()
+        gsm.unregisterUtility(zcmlconfig, IZCMLSolrConnectionConfig)
+
     def testConnectionConfigurationViaZCML(self):
-        from collective.solr import tests
-        self.layer.loadZCML('data/solr.zcml', package=tests)
+        loadZCMLString('''
+            <configure xmlns:solr="http://namespaces.plone.org/solr">
+                <solr:connection host="127.0.0.23" port="3898" base="/foo" />
+            </configure>
+        ''')
         manager = queryUtility(ISolrConnectionManager)
         manager.setHost(active=True)        # also clears connection cache
         connection = manager.getConnection()
