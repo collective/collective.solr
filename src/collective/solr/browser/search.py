034480f5799d729b4223f948cc8188e6003e0ba4
Timo Stollenwerk <tisto@plone.org>
2015-07-02 12:52:03 +0200

    Implement minimal json api.

---
+++ src/collective/solr/browser/search.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import json
+
+from plone.app.search.browser import Search as PloneAppSearchBrowserView
+from Products.CMFCore.utils import getToolByName
+
+
+class Search(PloneAppSearchBrowserView):
+
+    def __call__(self):
+        """If Accept header is 'application/json' or the URL contains
+           'format=json', the @@search view will return a JSON response.
+           Otherwise we just return the standard plone.app.search view.
+        """
+        json_header = self.request.response.getHeader(
+            'Accept'
+        ) == 'application/json'
+        json_format = self.request.get('format') == 'json'
+        if json_header or json_format:
+            self.request.response.setHeader(
+                'Content-Type',
+                'application/json; charset=utf-8'
+            )
+            SearchableText = self.request.get('SearchableText')
+            result = []
+            if SearchableText:
+                catalog = getToolByName(self.context, 'portal_catalog')
+                result = [
+                    {
+                        'id': x.id,
+                        'title': x.Title,
+                        'url': x.getURL(),
+                    }
+                    for x in catalog(SearchableText=SearchableText)
+                ]
+            return json.dumps(result, indent=2, sort_keys=True)
+        return super(PloneAppSearchBrowserView, self).__call__()
---
+++ src/collective/solr/tests/test_browser_search.py
@@ -0,0 +1,100 @@
+# -*- coding: UTF-8 -*-
+from zope.interface import directlyProvides
+from collective.solr.browser.interfaces import IThemeSpecific
+from collective.solr.testing import COLLECTIVE_SOLR_INTEGRATION_TESTING
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from zope.component import getMultiAdapter
+
+import json
+import unittest
+
+
+class SuggestTermsViewIntegrationTest(unittest.TestCase):
+
+    layer = COLLECTIVE_SOLR_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+        self.request['ACTUAL_URL'] = self.portal.absolute_url()
+        directlyProvides(self.request, IThemeSpecific)
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+
+    def test_search_view_returns_plone_app_search_view(self):
+        view = getMultiAdapter(
+            (self.portal, self.request),
+            name="search"
+        )
+        self.assertTrue(view)
+
+    def test_search_view_with_json_accept_header(self):
+        self.request.response.setHeader('Accept', 'application/json')
+        view = getMultiAdapter(
+            (self.portal, self.request),
+            name="search"
+        )
+        view = view.__of__(self.portal)
+        self.assertEqual(json.loads(view()), [])
+
+    def test_search_view_with_format_json(self):
+        self.request.set('format', 'json')
+        view = getMultiAdapter(
+            (self.portal, self.request),
+            name="search"
+        )
+        view = view.__of__(self.portal)
+        self.assertEqual(json.loads(view()), [])
+
+    def test_search_view_without_param(self):
+        self.request.set('format', 'json')
+        view = getMultiAdapter(
+            (self.portal, self.request),
+            name="search"
+        )
+        view = view.__of__(self.portal)
+        self.assertTrue(view())
+        self.assertEqual(json.loads(view()), [])
+
+    def test_search_view_with_empty_param(self):
+        self.request.set('format', 'json')
+        self.request.set('SearchableText', '')
+        view = getMultiAdapter(
+            (self.portal, self.request),
+            name="search"
+        )
+        view = view.__of__(self.portal)
+        self.assertTrue(view())
+        self.assertEqual(json.loads(view()), [])
+
+    def test_search_view(self):
+        self.portal.invokeFactory(
+            'Document',
+            id='doc1',
+            title='My First Document'
+        )
+        self.request.set('format', 'json')
+        self.request.set('SearchableText', 'First')
+
+        view = getMultiAdapter(
+            (self.portal, self.request),
+            name="search"
+        )
+        view = view.__of__(self.portal)
+
+        self.assertEqual(
+            len(json.loads(view())),
+            1
+        )
+        self.assertEqual(
+            json.loads(view())[0]['id'],
+            u'doc1',
+        )
+        self.assertEqual(
+            json.loads(view())[0]['title'],
+            u'My First Document',
+        )
+        self.assertEqual(
+            json.loads(view())[0]['url'],
+            u'{}/doc1'.format(self.portal.absolute_url())
+        )
