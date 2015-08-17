a4507c430992303129311378d83314e5c872edea
Timo Stollenwerk <tisto@plone.org>
2015-08-17 18:08:13 +0200

    Next version will be 4.2.0.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os.path
 from setuptools import setup
 from setuptools import find_packages
 
-version = '4.1.1.dev0'
+version = '4.2.0.dev0'
 long_description = \
     open('README.rst').read() + '\n' + \
     open('CHANGES.rst').read() + \
