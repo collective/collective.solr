a18c08bd6788dea54cb8f982f7d91aa19ade5158
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2015-09-16 09:48:01 +0200

    Enable partial reindex assertions.

--- src/collective/solr/tests/test_indexer.py
+++ src/collective/solr/tests/test_indexer.py
@@ -109,25 +109,20 @@ class QueueIndexerTests(TestCase):
         output = fakehttp(self.mngr.getConnection(), response)
         self.proc.index(foo)
         self.assert_(str(output).find(
-            '<field name="price">42.0</field>') > 0, '"price" data not found')
+            '<field name="price" update="set">42.0</field>') > 0,
+            '"price" data not found')
         # then only a subset...
         response = getData('add_response.txt')
         output = fakehttp(self.mngr.getConnection(), response)
         self.proc.index(foo, attributes=['id', 'name'])
         output = str(output)
         self.assert_(
-            output.find('<field name="name">foo</field>') > 0,
+            output.find('<field name="name" update="set">foo</field>') > 0,
             '"name" data not found'
         )
         # at this point we'd normally check for a partial update:
-        #   self.assertEqual(output.find('price'), -1, '"price" data found?')
-        #   self.assertEqual(output.find('42'), -1, '"price" data found?')
-        # however, until SOLR-139 has been implemented (re)index operations
-        # always need to provide data for all attributes in the schema...
-        self.assert_(
-            output.find('<field name="price">42.0</field>') > 0,
-            '"price" data not found'
-        )
+        self.assertEqual(output.find('price'), -1, '"price" data found?')
+        self.assertEqual(output.find('42'), -1, '"price" data found?')
 
     def testDateIndexing(self):
         foo = Foo(id='zeidler', name='andi', cat='nerd',
