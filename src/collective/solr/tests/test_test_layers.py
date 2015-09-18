96c7d004f257fdf8385cadbab190179c82d40fe3
Patrick Gerken <patrick.gerken@zumtobelgroup.com>
2015-09-18 11:37:36 +0200

    Fix tests

--- src/collective/solr/tests/test_test_layers.py
+++ src/collective/solr/tests/test_test_layers.py
@@ -21,18 +21,18 @@ class TestLayerTests(TestCase):
     def testRandomPorts(self):
         random.seed('Reproducable randomness')
         for random_port in self.expected_random_values:
-            layer = SolrLayer()
+            layer = SolrLayer(solr_port='RANDOM')
             self.assertEqual(random_port, layer.solr_port)
 
     def testRandomPortsSkipUsedPorts(self):
         random.seed('Reproducable randomness')
         for random_port in self.expected_random_values[:8]:
-            layer = SolrLayer()
+            layer = SolrLayer(solr_port='RANDOM')
             self.assertEqual(random_port, layer.solr_port)
         a_socket = socket.socket(socket.AF_INET)
         try:
             a_socket.bind(('127.0.0.1', self.expected_random_values[8]))
-            layer = SolrLayer()
+            layer = SolrLayer(solr_port='RANDOM')
             self.assertEqual(self.expected_random_values[9], layer.solr_port)
         finally:
             a_socket.close()
