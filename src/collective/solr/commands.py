5ac4dbf07089fc90fbc379bcc43ef95cf5161e1a
Thomas Schorr <t_schorr@gmx.de>
2015-11-07 10:07:29 +0100

    do not rely on positional args; changelog

--- src/collective/solr/commands.py
+++ src/collective/solr/commands.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import argparse
 from collective.solr.interfaces import ISolrConnectionManager
 from collective.solr.browser.maintenance import SolrMaintenanceView
 from zope.component import queryUtility
@@ -13,9 +14,15 @@ logger = logging.getLogger()
 
 
 def _get_site(app, args):
-    name = None
-    if len(args) > 2:
-        name = args[-1]
+    # Zope.Startup.zopectl.ZopeCmd.run_entrypoint promises to pass the entry point's
+    # name as the first argument and any further arguments after that, but that does not 
+    # work with plone.recipe.zope2instance. Using positional arguments therefore
+    # is unreliable - resolve to using a flag.
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--plonesite', help='Name of the Plone site', default=None)
+    namespace, unused = parser.parse_known_args(args)
+    name = namespace.plonesite
+    if name is not None:
         if name not in app:
             logger.error("Specified site '%s' not found in database." % name)
             sys.exit(1)
@@ -43,8 +50,8 @@ def _solr_connection():
 
 def solr_clear_index(app, args):
     """Removes all data from a Solr index. Equivalent to removing the
-    `data/index` directory while Solr is stopped. You can optionally specify
-    the id of the Plone site as the first command line argument.
+    `data/index` directory while Solr is stopped.
+    You can optionally specify the id of the Plone site with --plonesite <siteid>.
     """
     _get_site(app, args)  # calls setSite so queryUtility works
     conn = _solr_connection()
@@ -55,7 +62,8 @@ def solr_clear_index(app, args):
 def solr_reindex(app, args):
     """Reindex Solr. This is equivalent to /@@solr-maintenance/reindex, but
     can handle more documents. Using reindex from the browser will stop
-    eventually if there are too many documents, leaving the index incomplete
+    eventually if there are too many documents, leaving the index incomplete.
+    You can optionally specify the id of the Plone site with --plonesite <siteid>.
     """
     site = makerequest(_get_site(app, args))
     mv = SolrMaintenanceView(site, site.REQUEST)
