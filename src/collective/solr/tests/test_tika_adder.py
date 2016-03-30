c789f4d9f52ba42f1be55c60071315c7e18dba01
Christian Klinger <ck@novareto.de>
2016-03-30 12:53:13 +0200

    start working implementation for binary-adder + tika support

---
+++ src/collective/solr/tests/test_tika_adder.py
@@ -0,0 +1,46 @@
+from unittest import TestCase, defaultTestLoader
+from plone.namedfile.file import NamedBlobFile
+from Products.Archetypes.interfaces import IBaseObject
+from zope.interface import alsoProvides
+from zope.component import queryAdapter
+from plone.namedfile.tests.test_blobfile import registerUtilities
+from plone.namedfile.tests.base import NamedFileLayer
+from collective.solr.testing import (
+    COLLECTIVE_SOLR_INTEGRATION_TESTING
+)
+
+
+
+from collective.solr.interfaces import ISolrAddHandler
+class BinaryAdderTests(TestCase):
+
+    layer = COLLECTIVE_SOLR_INTEGRATION_TESTING
+
+    def setUp(self):
+        registerUtilities()
+
+    def test_binary_archetypes(self):
+        class MyField(object):
+
+            content = NamedBlobFile('BLA')
+            from Products.Archetypes import atapi
+            content = atapi.FileField('ba')
+
+            def get(self, context=None):
+                return self.content
+
+            def getPrimaryField(self):
+                return self
+
+            
+
+        obj = MyField()
+        alsoProvides(obj, IBaseObject)
+        adder = queryAdapter(obj, ISolrAddHandler, 'File')
+        adder.getpath()
+        self.assertEqual(1,2)
+
+
+def test_suite():
+    return defaultTestLoader.loadTestsFromName(__name__)
+
