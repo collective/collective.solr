a5992810f5855b7b81218dc65d5887f97589b289
Timo Stollenwerk <tisto@plone.org>
2016-04-13 15:38:28 +0200

    Attempt to fix ReST problem with pypi page.

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import find_packages
 version = '5.0.1.dev0'
 long_description = \
     open('README.rst').read() + '\n' + \
-    open('CHANGES.rst').read() + \
-    open(os.path.join('docs', 'credits.rst')).read() + \
+    open('CHANGES.rst').read() + '\n' + \
+    open(os.path.join('docs', 'credits.rst')).read() + '\n' + \
     open(os.path.join('docs', 'contributors.rst')).read(),
 
 setup(
