d83c761714a1020ab37b2619bf5198c9868f7e43
Tom Gross <itconsense@gmail.com>
2016-11-20 11:35:38 +0100

    add behavior and german translation
    fix tests and more translations
    
    static code analysis

---
+++ src/collective/solr/behaviors.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from collective.solr import SolrMessageFactory as _
+from plone.autoform.interfaces import IFormFieldProvider
+from plone.supermodel import directives
+from plone.supermodel import model
+from zope import schema
+from zope.interface import provider
+
+
+@provider(IFormFieldProvider)
+class ISolrFields(model.Schema):
+    """ Social Media integration.
+    """
+
+    directives.fieldset('categorization',)
+
+    showinsearch = schema.Bool(
+        required=False,
+        default=True,
+        missing_value=True,
+        title=_('label_showinsearch', default=u"Show in search"),
+        description=_('help_showinsearch', default=''),
+    )
+
+    searchwords = schema.List(
+        default=[],
+        missing_value=[],
+        title=_('label_searchwords', default=u"Search words"),
+        value_type=schema.TextLine(),
+        description=_(
+            'help_searchwords',
+            u"Specify words for which this item will show up "
+            u"as the first search result. Multiple words can be "
+            u"specified on new lines."
+        ),
+    )
+
+# EOF
---
+++ src/collective/solr/tests/test_behavior.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import unittest
+from collective.solr.testing import LEGACY_COLLECTIVE_SOLR_INTEGRATION_TESTING
+from collective.solr.behaviors import ISolrFields
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from plone.dexterity.fti import DexterityFTI
+
+
+class BehaviorsTestCase(unittest.TestCase):
+
+    layer = LEGACY_COLLECTIVE_SOLR_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+
+        fti = DexterityFTI('My Dexterity Container')
+        self.portal.portal_types._setObject('My Dexterity Container', fti)
+        fti.klass = 'plone.dexterity.content.Container'
+        fti.schema = 'zope.interface.Interface'
+        fti.behaviors = ('collective.solr.behaviors.ISolrFields',)
+        self.portal.invokeFactory('My Dexterity Container', 'child')
+        self.child = self.portal['child']
+
+    def test_behavior_showinsearch(self):
+        self.assertTrue(ISolrFields(self.child).showinsearch)
+
+    def test_behavior_searchwords(self):
+        self.assertEqual(ISolrFields(self.child).searchwords, [])
+
+# EOF
