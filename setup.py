a1e98890027b9ad1dab70acb3f755c4dd8be72ed
Timo Stollenwerk <tisto@plone.org>
2019-03-21 06:28:59 +0100

    Back to development: 7.0.0a3
    [ci skip]

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from setuptools import find_packages
 
-version = '7.0.0a2'
+version = '7.0.0a3-dev.0'
 
 long_description = '\n\n'.join([
     open('README.rst').read(),
