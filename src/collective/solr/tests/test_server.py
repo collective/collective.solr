279f6d913ebaef479e9f54ce855132c1f1bf38ad
Manuel Reinhardt <reinhardt@syslab.com>
2019-07-08 15:28:14 +0200

    Tests: make sure we don't have old data queued. On Python 2 the test actually passes despite the old data, but on Python 3 it fails, probably because the order of the data differs.

--- src/collective/solr/tests/test_server.py
+++ src/collective/solr/tests/test_server.py
@@ -310,6 +310,11 @@ class SolrMaintenanceTests(TestCase):
                                required=(iface,),
                                name='Image')
 
+        manager = getUtility(ISolrConnectionManager)
+        conn = manager.getConnection()
+        # make sure we don't have old data queued
+        conn.abort()
+
         # ignore_exceptions=False should raise the handler's exception,
         # thereby aborting the reindex tx
         maintenance = self.portal.unrestrictedTraverse('solr-maintenance')
