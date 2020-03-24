34fb1665de4d532be53562fd6cfb00d2bb678b2a
Timo Stollenwerk <stollenwerk@kitconcept.com>
2020-03-24 05:09:14 +0100

    Back to development: 8.0.0a9

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from setuptools import find_packages
 
-version = "8.0.0a8"
+version = "8.0.0a9.dev0"
 
 long_description = "\n\n".join(
     [open("README.rst").read(), open("CHANGES.rst").read()])
