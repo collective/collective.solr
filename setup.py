0c0acedce4a8bb5e865137bb0c50f4bd5a6f9711
Timo Stollenwerk <stollenwerk@kitconcept.com>
2020-12-16 09:29:08 +0100

    Back to development: 8.4.2
    [ci skip]

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from setuptools import find_packages
 
-version = "8.4.1"
+version = "8.4.2.dev0"
 
 long_description = "\n\n".join(
     [open("README.rst").read(), open("CHANGES.rst").read()])
