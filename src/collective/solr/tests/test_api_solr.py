349566a5095a086d9c44a1f6067bd7b6dc25a3eb
Timo Stollenwerk <stollenwerk@kitconcept.com>
2021-11-23 13:45:13 +0100

    Add test with q param

--- src/collective/solr/tests/test_api_solr.py
+++ src/collective/solr/tests/test_api_solr.py
@@ -3,7 +3,10 @@ from collective.solr.testing import LEGACY_COLLECTIVE_SOLR_FUNCTIONAL_TESTING
 from collective.solr.utils import activate
 from plone.app.testing import SITE_OWNER_NAME
 from plone.app.testing import SITE_OWNER_PASSWORD
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
 from plone.restapi.testing import RelativeSession
+from plone import api
 
 import unittest
 import transaction
@@ -17,6 +20,14 @@ class SolrMaintenanceTests(unittest.TestCase):
         self.portal = self.layer["portal"]
         self.request = self.layer["request"]
         self.portal_url = self.portal.absolute_url()
+
+        setRoles(self.portal, TEST_USER_ID, ["Manager"])
+        self.doc = api.content.create(
+            container=api.portal.get(),
+            type="Document",
+            title="Colorless Green Ideas",
+        )
+
         activateAndReindex(self.portal)
 
         transaction.commit()
@@ -27,9 +38,17 @@ class SolrMaintenanceTests(unittest.TestCase):
     def tearDown(self):
         activate(active=False)
 
-    def test_foo(self):
+    def test_solr_endpoint_without_q_parameter(self):
         response = self.api_session.get("/@solr")
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.headers.get("Content-Type"), "application/json")
         self.assertEqual(5, len(response.json()))
+
+    def test_solr_endpoint(self):
+        response = self.api_session.get("/@solr?q=Colorless")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.headers.get("Content-Type"), "application/json")
+        self.assertEqual(1, len(response.json()))
+        self.assertEqual(response.json()[0]["Title"], "Colorless Green Ideas")
